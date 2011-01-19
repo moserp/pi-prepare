b3d770260ffdcdc567ee23658fe9d64cdab75e85
Paul Moser <paul.t.moser@gmail.com>
2011-01-19 23:50:24 +0000

    Install Chef gem on CentOS

--- centos.rb
+++ centos.rb
@@ -3,10 +3,48 @@ require 'common'
 policy :centos, :roles => :app do
   requires :mtu
   requires :yum_sources
+  requires :chef_client
 end
 
 package :yum_sources do
   transfer 'CentOS-Base.repo', '/etc/yum.repos.d/CentOS-Base.repo'
+end
+
+package :chef_client do
+  gem 'chef'
+  requires :ruby
+  requires :rubygems
+  requires :make
+end
+
+package :ruby do
+  yum %w(ruby ruby-shadow ruby-ri ruby-rdoc ruby-devel ruby-static)
+  requires :gcc
+  requires :epel
+  requires :elff
+end
+
+package :rubygems do
+  source 'http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz' do
+    custom_install 'ruby setup.rb --no-format-executable'
+  end
+  requires :ruby
+end
+
+package :make do
+  yum 'make'
+end
+
+package :gcc do
+  yum %w(gcc gcc-c++)
+end
+
+package :epel do
+  rpm 'http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm'
+end
+
+package :elff do
+  rpm 'http://download.elff.bravenet.com/5/i386/elff-release-5-3.noarch.rpm'
 end
 
 deployment do
@@ -14,5 +52,11 @@ deployment do
     set :user, 'root'
     role :app, '109.144.14.xx', :primary => true
     default_run_options[:pty] = true
+  end
+  # source based package installer defaults
+  source do
+    prefix   '/usr/local'           # where all source packages will be configured to install
+    archives '/usr/local/sources'   # where all source packages will be downloaded to
+    builds   '/usr/local/build'     # where all source packages will be built
   end
 end
